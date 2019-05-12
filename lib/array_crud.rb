feb5179b2885d8fe4e4c03f57058a7f86d06b4e7
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-12 20:09:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,17 +7,17 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = ["chicken", "beef","pork"]
-  my_array.push("fish")
+  my_array = array
+  my_array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = ["chicken", "beef","pork"]
+  my_array = array
   my_array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  my_array = ["chicken", "beef","pork"]
+  my_array = array
   my_array.pop
 end
 
@@ -27,16 +27,16 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  my_array = ["chicken", "beef","pork"]
+  my_array = array
   my_array[0]
 end
 
 def retrieve_first_element_from_array(array)
-  my_array = ["chicken", "beef","pork"]
+  my_array = array
   my_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  my_array = ["chicken", "beef","pork"]
+  my_array = array
   my_array[-1]
 end
